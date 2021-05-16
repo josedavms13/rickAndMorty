0a653326a65d04178c606094a949fb94bca1ab5a
josedavms13 <josedav0ms13@gmail.com>
2021-05-15 23:43:21 -0500

    FEAT:
    --->Amount of cards to show System works
    next: Display Card List

--- src/VIEWS/residentsListCard.js
+++ src/VIEWS/residentsListCard.js
@@ -4,6 +4,13 @@ import ResidentListItem from "../components/residentListItem";
 
 const ResidentsListCard = ({data})=>{
 
+    const [cardToggle, SetCardToggle] = useState(false);
+
+    const [overallData, SetOverallData] = useState(null)
+    const [amountOfDataToDisplay, SetAmountOfDataToDisplay]= useState(0)
+    const [dataToDisplay, SetDataToDisplay] = useState(null);
+
+
     const [characterName, SetCharacterName] = useState(null)
     const [characterSpecies, SetCharacterSpecies] = useState(null)
     const [characterGender, SetCharacterGender] = useState(null)
@@ -14,20 +21,44 @@ const ResidentsListCard = ({data})=>{
 
         console.log(data);
 
-        SetCharacterName(data[0].name);
-        SetCharacterSpecies(data[0].species);
-        SetCharacterGender(data[0].gender);
-        SetCharacterStatus(data[0].status);
-        SetCharacterPicture(data[0].image);
+        if(data){
 
-        console.log(characterName)
-
+            SetOverallData(data);
+        }
 
 
     },[data])
 
 
 
+
+
+    function displayCards(){
+        if(amountOfDataToDisplay === 0){
+            console.log('display all data')
+        }
+        else {
+            const maxNumber = () => {
+
+                if (amountOfDataToDisplay > overallData.length) {
+                    return overallData.length
+                } else {
+                    return amountOfDataToDisplay
+                }
+
+
+            }
+
+            console.log(maxNumber())
+        }
+
+        // SetCharacterName(overallData[0].name);
+        // SetCharacterSpecies(overallData[0].species);
+        // SetCharacterGender(overallData[0].gender);
+        // SetCharacterStatus(overallData[0].status);
+        // SetCharacterPicture(overallData[0].image);
+
+    }
 
 
 
@@ -39,12 +70,29 @@ const ResidentsListCard = ({data})=>{
 
         <div className={'resident-list-container'}>
 
+            <div className="display-settings">
+                <div className="display-instructions">
+                    <label>Set the number of items to display</label>
+                    <p>If 0, it will display all results</p>
+                </div>
+                <div className="form">
+                    <input type="number" min={0} defaultValue={0} onChange={(e) => {
+                        SetAmountOfDataToDisplay(Number(e.target.value))
+                    }}/>
+                    <button onClick={displayCards}>Submit!</button>
+                </div>
 
-            <ResidentListItem name={characterName} species={characterSpecies} gender={characterGender} status={characterStatus} picture={characterPicture}/>
+
+                <div className="go-back-container">
+                    <button>Go Back</button>
+                </div>
+            </div>
+            {cardToggle&&<div className="cards-container">
+
+                <ResidentListItem name={characterName} species={characterSpecies} gender={characterGender}
+                                  status={characterStatus} picture={characterPicture}/>
+            </div>}
         </div>
-
-
-
 
 
     )
